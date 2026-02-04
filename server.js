3660d02ab944840b61409e206702ee927e603d47
chilllantern4-boop <chilllantern4@gmail.com>
2026-02-05 01:55:39 +0200

    Update server.js

--- server.js
+++ server.js
@@ -6,13 +6,29 @@ const app = express();
 const server = http.createServer(app);
 
 const io = new Server(server, {
-  cors: { origin: "*" }
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"]
+  }
+});
+
+// 👇 ADD THIS ROUTE (VERY IMPORTANT)
+app.get("/", (req, res) => {
+  res.send("Socket.IO server is running");
 });
 
 io.on("connection", socket => {
+  console.log("User connected");
+
   socket.on("message", msg => {
     io.emit("message", msg);
   });
+
+  socket.on("disconnect", () => {
+    console.log("User disconnected");
+  });
 });
 
-server.listen(process.env.PORT || 3000);
+server.listen(process.env.PORT || 3000, () => {
+  console.log("Server started");
+});
