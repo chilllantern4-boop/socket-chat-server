495f8891fe15920c6d2221e7b9fb64dd3c4eb27f
chilllantern4-boop <chilllantern4@gmail.com>
2026-02-05 02:27:10 +0200

    Update server.js

--- server.js
+++ server.js
@@ -6,41 +6,21 @@ const app = express();
 const server = http.createServer(app);
 
 const io = new Server(server, {
-  cors: {
-    origin: "*",
-    methods: ["GET", "POST"]
-  }
+  cors: { origin: "*" }
 });
 
-// 👇 ADD THIS ROUTE (VERY IMPORTANT)
-app.get("/", (req, res) => {
-  res.send("Socket.IO server is running");
-});
+// Route to check server
+app.get("/", (req, res) => res.send("Socket.IO server running"));
 
+// Broadcast messages
 io.on("connection", socket => {
   console.log("User connected");
 
   socket.on("message", msg => {
-    io.emit("message", msg);
+    io.emit("message", msg); // send to everyone
   });
 
-  socket.on("disconnect", () => {
-    console.log("User disconnected");
-  });
+  socket.on("disconnect", () => console.log("User disconnected"));
 });
 
-server.listen(process.env.PORT || 3000, () => {
-  console.log("Server started");
-});
-
-const messagesHistory = [];
-
-io.on("connection", socket => {
-  // Send old messages to the newly connected client
-  messagesHistory.forEach(msg => socket.emit("message", msg));
-
-  socket.on("message", msg => {
-    messagesHistory.push(msg); // store it
-    io.emit("message", msg);   // send to everyone
-  });
-});
+server.listen(process.env.PORT || 3000, () => console.log("Server started"));
